78c2891f257495de3d6def9cfaa53e38f9535c08
RYpLi <zjanasek27@gmail.com>
2024-07-30 16:00:18 +0200

    sprite update test

---
+++ Assets/Scripts/spriteBillboard.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class spriteBillboard : MonoBehaviour
+{
+    [SerializeField] bool Axis = true;
+
+    private void LateUpdate()
+    {
+        if (Axis)
+        {
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        } else
+        {
+            transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+}
